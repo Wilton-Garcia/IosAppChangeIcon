d81b69f5816c0365a43d34fa9b06e3cf9b17e98c
Wilton Garcia <wiltongarciaweb@gmail.com>
2025-04-07 15:38:40 -0300

    Base app

---
+++ IosAppChnageIcon/IosAppChnageIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosAppChnageIcon
+//
+//  Created by Wilton Garcia on 07/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .purple
+    }
+
+
+}
+
